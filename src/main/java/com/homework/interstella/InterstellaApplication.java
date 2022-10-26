b552a8a47cd4b37a9ba700a9dfaccade256dcf78
snsnalal <jujujo79@gmail.com>
2022-10-26 21:20:15 +0900

    Initial commit

---
+++ src/main/java/com/homework/interstella/InterstellaApplication.java
@@ -0,0 +1,13 @@
+package com.homework.interstella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterstellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterstellaApplication.class, args);
+	}
+
+}
