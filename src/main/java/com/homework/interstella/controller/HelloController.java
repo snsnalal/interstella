c9e5247706818057a97d8a09998bd82f9f654ad9
snsnalal <jujujo79@gmail.com>
2022-10-28 00:12:56 +0900

    webhook test

--- src/main/java/com/homework/interstella/controller/HelloController.java
+++ src/main/java/com/homework/interstella/controller/HelloController.java
@@ -3,8 +3,10 @@ package com.homework.interstella.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "interstella-0.0.1-SNAPSHOT/")
 public class HelloController {
 
     @GetMapping("/")
