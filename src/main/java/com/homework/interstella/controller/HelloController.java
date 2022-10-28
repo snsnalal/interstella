47a7f35ca1a11bfe54488bcab540c7e53ef7b11c
snsnalal <jujujo79@gmail.com>
2022-10-29 04:58:31 +0900

    change thymeleaf to jsp

--- src/main/java/com/homework/interstella/controller/HelloController.java
+++ src/main/java/com/homework/interstella/controller/HelloController.java
@@ -3,10 +3,8 @@ package com.homework.interstella.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/interstella-0.0.1-SNAPSHOT")
 public class HelloController {
 
     @GetMapping("/")
