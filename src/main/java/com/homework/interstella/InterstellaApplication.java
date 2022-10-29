6be2453ebcb7b1b1824d515ee9f2d52856eea084
snsnalal <jujujo79@gmail.com>
2022-10-29 17:52:34 +0900

    change for jsp

--- src/main/java/com/homework/interstella/InterstellaApplication.java
+++ src/main/java/com/homework/interstella/InterstellaApplication.java
@@ -2,12 +2,18 @@ package com.homework.interstella;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InterstellaApplication {
+public class InterstellaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterstellaApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(InterstellaApplication.class);
+	}
 }
