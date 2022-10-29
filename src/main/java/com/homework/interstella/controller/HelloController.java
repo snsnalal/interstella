71cdfaa5b1e7a1bbdec709bc80768b297121861d
snsnalal <jujujo79@gmail.com>
2022-10-29 18:08:57 +0900

    change for jsp

--- src/main/java/com/homework/interstella/controller/HelloController.java
+++ src/main/java/com/homework/interstella/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.homework.interstella.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/interstella-0.0.1-SNAPSHOT")
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping
     public String printHello(Model model){
         model.addAttribute("hello", "hello world!");
         return "hello";
